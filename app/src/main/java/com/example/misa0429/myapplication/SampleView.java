d0893893fefbc7e21dc434c69b5e0736a8e2ac48
graceoym <r.misa.w@gmail.com>
2014-09-12 17:53:45 +0900

    0912 1753

--- app/src/main/java/com/example/misa0429/myapplication/SampleView.java
+++ app/src/main/java/com/example/misa0429/myapplication/SampleView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -26,6 +27,40 @@ class SampleView extends View {
     //5つのレーンのX座標
     int[] lane = new int[]{0, 216, 432, 648, 846};
 
+    int touch_count = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        float x = ev.getX();
+        float y = ev.getY();
+        String action = "";
+
+        if (viewWidth / 2 - width / 2 <= x && x <= viewWidth / 2 + width / 2 && playerY <= y && y <= playerY + viewHeight) {
+            switch (ev.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    action = "Touch Down";
+                    touch_count += 1;
+                    //    taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher_reverse);
+                    playerVY = 0;
+
+                    //playerVY = -1920;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    action = "Touch Move";
+                    break;
+                case MotionEvent.ACTION_UP:
+                    action = "Touch Up";
+//                  //  taxi = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    action = "Touch Cancel";
+                    break;
+            }
+           // Log.d("TAG", "action : " + action + " count : " + touch_count);
+        }else {
+        }
+        return true;
+    }
 
     @Override
     public void onWindowFocusChanged(boolean hasWindowFocus) {
@@ -70,14 +105,15 @@ class SampleView extends View {
         playerY += playerVY;
 
         //上まで行ったら下に戻る動き
-       if(playerY < 0) {
+       if(playerY < -height) {
            playerY = viewHeight;
            r = new java.util.Random ().nextInt (4);
            playerX = lane[r];
-       }
+
+        }
+
         //描画処理
         c.drawBitmap (taxi, playerX, playerY, paint);
-
 
         // ループ処理、スピードの調整（ミリ秒）
             postInvalidateDelayed(50);
